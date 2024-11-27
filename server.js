5ca885ed7c8406e7768d9d7c0d4b19dc90b04815
Trong Nguyen <trongnguyen.main@gmail.com>
2024-11-27 09:42:22 +0700

    os module exports and require statement nodejs

---
+++ server.js
@@ -0,0 +1,21 @@
+const os = require('os');
+const path = require('path');
+const { add, multiply } = require('./math');
+
+console.log(add(12, 5));
+console.log(multiply(12, 5));
+
+
+// console.log(os.type());
+// console.log(os.version());
+// console.log(os.homedir());
+
+// console.log(__dirname);
+// console.log(__filename);
+
+// console.log(path.dirname(__filename));
+// console.log(path.basename(__filename));
+// console.log(path.extname(__filename));
+
+// console.log(path.parse(__filename));
+
